63b0de71d94c52dc5cd75906d41561c194c510a7
lutomlin <lutomlin@users.noreply.github.com>
2026-08-17 04:39:59 +0000

    deploy: 6718290f7e37694c272a20f6e861e997fff59a15

---
+++ _app/immutable/chunks/paths.9ab96db5.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_1ojl6pm)==null?void 0:s.base)??"/timeless-jewels";var e;const t=((e=globalThis.__sveltekit_1ojl6pm)==null?void 0:e.assets)??l;export{t as a,l as b};
